cd3f3e87c75a09fb567148f088dcf8c3dbd7e67b
Jordan2-3 <55561231+Jordan2-3@users.noreply.github.com>
2019-09-22 15:27:51 -0400

    Add files via upload

---
+++ NoblesJ_Lab2.java
@@ -0,0 +1,49 @@
+//Jordan Nobles 00444118 9/18/19 12:07pm
+
+public class NoblesJ_Lab2
+{
+   public static void main(String[] args) 
+   {
+
+//Part A Variabales and Literals
+
+int distance, time; 
+
+distance = 350;
+time = 8;
+
+System.out.println(" Part A: " + "\"" + "Variable and Literals" + "\"" + '\n' );
+System.out.println( "the distance between two cities is " +
+                   distance + " miles and it takes " + time +
+                   "hours to drive between them." + '\n');
+                   
+//Part B Arthimetic Operators 1
+double total, cost, tax;
+final double sale_tax_rate = .0925; //can't use percent symbol 
+
+cost = 320;
+tax = sale_tax_rate * cost;
+total = cost + tax;
+
+System.out.println( " Part B: " + "\"" + "Arthimetic Operators 1" + "\"" + '\n' );
+System.out.print(" Cost is $" + cost + ", the sales tax is $" + tax +
+                  " and the total is $" + total + '.' = '\n');  
+
+//Part C Arithmetic Operator 2
+
+int dividend, divisor, quotient, remainder;
+
+dividend = 23;
+divisor = 7;
+
+quotient = dividend / divisor;
+remainder = dividend % divisor;
+
+System.out.println( " Part C: " + "\"" + "Arithmetic Operator 2" + "\"" + '\n' );
+System.out.println( " When " + dividend + "is divided by" + divisor +
+                     " the quotient is 
+
+
+      }
+}
+                   
